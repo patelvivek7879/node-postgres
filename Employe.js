ca4d8358f13952c6523364d127737eef9ca87010
Vivek Patel <vivek.patel@tmdc.io>
2021-09-24 19:14:19 +0530

    try-catch added

---
+++ Employe.js
@@ -0,0 +1,16 @@
+class Employee {
+    constructor(code, name, age) {
+        this.code = code;
+        this.name = name;
+        this.age = age;
+    }
+    getName() {
+        return this.name;
+    }
+    getAge() {
+        return this.age;
+    }
+    getCode() {
+        return this.code;
+    }
+}
--- Employees.js
+++ Employees.js
@@ -1,25 +1,6 @@
 // const { Client } = require("pg");
-const express = require("express");
-const router = express.Router();
+const router = require('express').Router();
 const { Client } = require('pg');
-
-// const url = require('url');
-// const queryString = require('querystring');
-
-//const bodyParser = require('body-parser');
-//const urlEncodedBodyParser = bodyParser({"extended": false});
-//app.use(bodyParser.urlencoded({"extended": false}));
-//app.use(bodyParser.json());
-
-// app.use(express.json());
-// app.use(express.urlencoded({
-//     extended: false
-// }));
-//app.use(express.static('public'));
-
-// app.get("/",(req, res)=>{
-//     res.redirect("/index.html");
-// });
 
 const client = new Client({
     "user": "postgres",
@@ -37,100 +18,70 @@ if (conn) {
     console.log('database connected ..')
 }
 
-class Employee {
-    constructor(code, name, age) {
-        this.code = code;
-        this.name = name;
-        this.age = age;
-    }
-    getName() {
-        return this.name;
-    }
-    getAge() {
-        return this.age;
-    }
-    getCode() {
-        return this.code;
-    }
-}
-
 router.get("/employees", async (req, res) => {
-    
+
     console.log("Base URL : ", req.baseUrl);
 
     var employees = [];
-    let result = await client.query("select * from employee");
-    result.rows.forEach((row) => {
-        employees.push(new Employee(row.code, row.name.trim(), row.age));
-    });
-    console.log(employees);
+    try {
+        const result = await client.query("select * from employee");
+        result.rows.forEach((row) => {
+            employees.push(new Employee(row.code, row.name.trim(), row.age));
+        });
+    } catch (error) {
+
+    }
     res.send(employees);
 })
 
-router.get("/getEmployeeByCode/:id", async (req,res)=>{
-    let code = req.params.id;
-    console.log(req.params);
-    let result = await client.query(`select * from employee where code=${code}`);
-    if(result.rows.length < 1){
-        res.status(404).send({
-            success: false,
-            message: "Employee not found"
-        })
-    }else if(result){
-    result.rows[0].name = result.rows[0].name.trim();
-    console.log(result.rows)
-    res.send(result.rows[0]);
-    }
-});
+router.get("/getEmployeeByCode/:id", async (req, res) => {
+    const { id } = req.params;
 
-router.get("/getEmployeeByName",async (req, res)=>{
-    let name = req.query.name;
-    console.log(name);
-    let result = await client.query(`select * from employee where name='${name}'`);
-    if(result.rows.length < 1){
-        res.status(404).send({
-            success: false,
-            message: "Employee not found"
-        });
-    }
-    else if(result){
-    result.rows[0].name=result.rows[0].name.trim()
-    res.send(result.rows);
-    }
-}); 
+    console.log(code);
+    try {
 
-//to handle any unappropriate request
-router.get("*",async (req,res)=>{
-  res.redirect("/error.html");
-    //res.redirect("back");
-    //res.redirect('..');
-});
 
-router.get("/",(req, res)=>{
-    var options = {
-        root: path.join(__dirname)
-    };
-
-    let fileName = "time1.js";
-    console.log('GETTING A FILE >>>>>>')
-    res.sendFile(fileName, options, function (err) {
-        if (err) {
-            next(err);
-        } else {
-            console.log('Sent:', fileName);
+        let result = await client.query(`select * from employee where code=${id}`);
+        if (result.rows.length < 1) {
+            res.status(404).send({
+                success: false,
+                message: "Employee not found"
+            })
+        } else if (result) {
+            result.rows[0].name = result.rows[0].name.trim();
+            console.log(result.rows)
+            res.send(result.rows[0]);
         }
-    });
+    } catch (error) {
+    }
+});
+
+router.get("/getEmployeeByName", async (req, res) => {
+    let { name } = req.query;
+    console.log(name);
+    try {
+        const result = await client.query(`select * from employee where name='${name}'`);
+        if (result.rows.length < 1) {
+            res.status(404).send({
+                success: false,
+                message: "Employee not found"
+            });
+        }
+        else if (result) {
+            result.rows[0].name = result.rows[0].name.trim()
+            res.send(result.rows);
+        }
+    } catch (error) {
+
+    }
+    res.send(result.rows);
 });
 
 router.post("/addEmployee", async (req, res) => {
 
-    //let code = req.body.code;
-    //let name = req.body.name;
-    //let age = req.body.age;
-    //let {...employee} = { code, name, age };
-
     let employee = req.body;
-    let emp = await client.query(`select code from employee where code=${employee.code}`);
+    try {
+        const emp = await client.query(`select code from employee where code=${employee.code}`);
 
     if (emp.rowCount === 1) {
         res.send({
@@ -140,7 +91,7 @@ router.post("/addEmployee", async (req, res) => {
         });
     }
 
-    let result = await client.query(`insert into employee values(${employee.code},'${employee.name}', ${employee.age})`);
+    const result = await client.query(`insert into employee values(${employee.code},'${employee.name}', ${employee.age})`);
 
     if (result.command === 'INSERT') {
         res.send({
@@ -153,44 +104,68 @@ router.post("/addEmployee", async (req, res) => {
             "success": false,
             "error": true
         });
+    }    
+    } catch (error) {
+        
     }
+    res.send({
+        success: true,
+        error: false
+    });
 });
 
 
 router.post("/updateEmployee", async (req, res) => {
+
     const { ...employee } = req.body;
+    try{
+        let result = await client.query(`select * from employee where code=${employee.code}`);
 
-    let result = await client.query(`select * from employee where code=${employee.code}`);
+        if (!result.rowCount) {
+            res.status(400).json("Invalid code, user does not exists.")
+        }
+        else {
+            result = await client.query(`update employee set name='${employee.name}', age=${employee.age} where code = ${employee.code}`);
+            console.log(result);
+            res.json({
+                "success": true,
+                "error": false
+            })
+        }
+    }catch(error){
 
-    if (!result.rowCount) {
-        res.status(400).json("Invalid code, user does not exists.")
     }
-    else {
-        result = await client.query(`update employee set name='${employee.name}', age=${employee.age} where code = ${employee.code}`);
-        console.log(result);
-
-        res.json({
-            "success": true,
-            "error": false
-        })
-    }
+    res.send({success: true,
+        error: false
+    });
 });
+
 router.post("/deleteEmployee", async (req, res) => {
     const { ...employee } = req.body;
-    let result = client.query(`select code from employee where code=${employee.code}`);
-    console.log(result);
-    if (result.rowCount) {
-        res.status(404).json({
-            "success": false,
-            "message": `emplyee with code ${employee.code} does not exists`
-        });
-    } else {
-        result = client.query(`delete from employee where code=${employee.code}`);
-        res.json({
-            "success": true,
-            "message": `Employee delete with code ${employee.code}`
-        });
+    try{
+        let result = client.query(`select code from employee where code=${employee.code}`);
+        console.log(result);
+        if (result.rowCount) {
+            res.status(404).json({
+                "success": false,
+                "message": `emplyee with code ${employee.code} does not exists`
+            });
+        } else {
+            result = client.query(`delete from employee where code=${employee.code}`);
+            res.json({
+                "success": true,
+                "message": `Employee delete with code ${employee.code}`
+            });
+        }
+    }catch(error){
+        
     }
+
+});
+
+//to handle any unappropriate request
+router.get("*", (req, res) => {
+    res.redirect("/error.html");
 });
 
 
